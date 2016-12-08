961d30357be9d919891bfc3f0335fc6502de11c5
Stjepan Zugcic <stzugcic@vsite.hr>
2016-12-08 21:12:57 +0100

    Riješili smo MainForm

--- PretplataNaDogadjaje/MainForm.Designer.cs
+++ PretplataNaDogadjaje/MainForm.Designer.cs
@@ -28,60 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox = new System.Windows.Forms.TextBox();
-            this.buttonDelete = new System.Windows.Forms.Button();
-            this.buttonClose = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 12);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "&Write some text:";
-            // 
-            // textBox
-            // 
-            this.textBox.Location = new System.Drawing.Point(101, 9);
-            this.textBox.Name = "textBox";
-            this.textBox.Size = new System.Drawing.Size(171, 20);
-            this.textBox.TabIndex = 1;
-            // 
-            // buttonDelete
-            // 
-            this.buttonDelete.Enabled = false;
-            this.buttonDelete.Location = new System.Drawing.Point(197, 35);
-            this.buttonDelete.Name = "buttonDelete";
-            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
-            this.buttonDelete.TabIndex = 2;
-            this.buttonDelete.Text = "&Delete";
-            this.buttonDelete.UseVisualStyleBackColor = true;
-            // 
-            // buttonClose
-            // 
-            this.buttonClose.Location = new System.Drawing.Point(197, 227);
-            this.buttonClose.Name = "buttonClose";
-            this.buttonClose.Size = new System.Drawing.Size(75, 23);
-            this.buttonClose.TabIndex = 3;
-            this.buttonClose.Text = "&Close";
-            this.buttonClose.UseVisualStyleBackColor = true;
-            // 
-            // MainForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.buttonClose);
-            this.Controls.Add(this.buttonDelete);
-            this.Controls.Add(this.textBox);
-            this.Controls.Add(this.label1);
-            this.Name = "MainForm";
-            this.Text = "Events";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox = new System.Windows.Forms.TextBox();
+			this.buttonDelete = new System.Windows.Forms.Button();
+			this.buttonClose = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 12);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(83, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "&Write some text:";
+			// 
+			// textBox
+			// 
+			this.textBox.BackColor = System.Drawing.SystemColors.Info;
+			this.textBox.Location = new System.Drawing.Point(101, 9);
+			this.textBox.Name = "textBox";
+			this.textBox.Size = new System.Drawing.Size(171, 20);
+			this.textBox.TabIndex = 1;
+			this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+			// 
+			// buttonDelete
+			// 
+			this.buttonDelete.Enabled = false;
+			this.buttonDelete.Location = new System.Drawing.Point(197, 35);
+			this.buttonDelete.Name = "buttonDelete";
+			this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+			this.buttonDelete.TabIndex = 2;
+			this.buttonDelete.Text = "&Delete";
+			this.buttonDelete.UseVisualStyleBackColor = true;
+			this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+			// 
+			// buttonClose
+			// 
+			this.buttonClose.Location = new System.Drawing.Point(197, 227);
+			this.buttonClose.Name = "buttonClose";
+			this.buttonClose.Size = new System.Drawing.Size(75, 23);
+			this.buttonClose.TabIndex = 3;
+			this.buttonClose.Text = "&Close";
+			this.buttonClose.UseVisualStyleBackColor = true;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.buttonClose);
+			this.Controls.Add(this.buttonDelete);
+			this.Controls.Add(this.textBox);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "Events";
+			//this.buttonClose += new System.EventHandler(this.buttonClose_click);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
--- PretplataNaDogadjaje/MainForm.cs
+++ PretplataNaDogadjaje/MainForm.cs
@@ -15,13 +15,28 @@ namespace Vsite.CSharp
         {
             InitializeComponent();
         }
+		// Dodati metodu za obradu događaja (event handler) koji će onemogućiti
+		// tipku buttonDelete ako nema upisanog teksta u kontroli textBox, odnosno omogućiti kada je neki tekst upisan.
 
-        // TODO: Dodati metodu za obradu događaja (event handler) koji će onemogućiti tipku buttonDelete ako nema upisanog teksta u kontroli textBox, odnosno omogućiti kada je neki tekst upisan.
+		private void textBox_TextChanged(object sender, EventArgs e)
+		{
+			buttonDelete.Enabled=textBox.Text.Length > 0;
+		}
+		// Dodati metodu za obradu događaja koji će na
+		//pritisak tipke buttonDelete obrisati sadržaj kontrole textBox.
+
+		private void buttonDelete_Click(object sender, EventArgs e)
+		{
+			textBox.Clear();
+		}
+
+		private void buttonClose_click(object sender, EventArgs e)
+		{
+			Close();
+		}
 
 
-        // TODO: Dodati metodu za obradu događaja koji će na pritisak tipke buttonDelete obrisati sadržaj kontrole textBox.
- 
 
-        // TODO: Dodati metodu za obradu događaja koji će na pritisak tipke buttonClose zatvoriti formu.
-    }
+		// TODO: Dodati metodu za obradu događaja koji će na pritisak tipke buttonClose zatvoriti formu.
+	}
 }
